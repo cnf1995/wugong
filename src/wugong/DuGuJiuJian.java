37cc87b0484c79b2db6e2380831298d60b4d9f30
宁锋 <caonife@126.com>
2019-11-03 01:52:29 +0800

    什么情况！

--- src/wugong/DuGuJiuJian.java
+++ src/wugong/DuGuJiuJian.java
@@ -7,8 +7,7 @@ public class DuGuJiuJian {
 		System.out.println("111111");
 		System.out.println("22333");
 		System.out.println("233333");
-		System.out.println("444444");
-		System.out.println("555555");
+		System.out.println("什么情况！！！");
 	}
 
 }
